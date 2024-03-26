6e24dfa7eb1abff1606b13841012326fec2d5687
Can NAMLI <namlimuhammetcan55@gmail.com>
2024-03-26 13:57:45 +0300

    Switch-case structure added for user choice

--- todo_list.go
+++ todo_list.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"os"
 )
 
 func main() {
@@ -14,6 +15,25 @@ func main() {
 		fmt.Println("4. Mark as Uncompleted")
 		fmt.Println("5. Exit")
 		fmt.Println("Option: ")
+
+		var choice int
+		fmt.Scanln(&choice)
+
+		switch choice {
+		case 1:
+			addTask()
+		case 2:
+			listTasks()
+		case 3:
+			markComp()
+		case 4:
+			markUncomp()
+		case 5:
+			fmt.Println("Exiting the program...")
+			os.Exit(0)
+		default:
+			fmt.Println("Invalid option!")
+		}
 	}
 }
 
