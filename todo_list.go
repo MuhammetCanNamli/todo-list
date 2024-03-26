5af5d40da252768df5c57b634b27ce5e258102f4
Can NAMLI <namlimuhammetcan55@gmail.com>
2024-03-26 13:49:09 +0300

    ToDo List user options have been created.

--- todo_list.go
+++ todo_list.go
@@ -1,5 +1,18 @@
 package main
 
+import (
+	"fmt"
+)
+
 func main() {
 
+	for {
+		fmt.Println("ToDo List")
+		fmt.Println("1. Add")
+		fmt.Println("2. Show")
+		fmt.Println("3. Mark as Completed")
+		fmt.Println("4. Mark as Uncompleted")
+		fmt.Println("5. Exit")
+		fmt.Println("Option: ")
+	}
 }
